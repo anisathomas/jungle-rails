4c0e88e80025abe573da16da35be3b45cbb20773
Anisa Thomas <anisathomas99@gmail.com>
2018-06-22 20:35:15 +0000

    edit to email functionality

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/example_mailer
 class OrderMailerPreview < ActionMailer::Preview
   def order_mail_preview
-    OrderMailer.order_email(User.first, Order.find(22))
+    OrderMailer.order_email(User.first, Order.find(23))
   end
 end
